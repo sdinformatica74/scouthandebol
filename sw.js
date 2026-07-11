d2edd600aa31550b799429ae172adec2adfce487
SD <247677353+sdinformatica74@users.noreply.github.com>
2026-07-10 21:26:38 -0300

    Update CACHE_NAME to scout-handebol-v17

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Scout Handebol - Service Worker
 // v11
-const CACHE_NAME = 'scout-handebol-v16';
+const CACHE_NAME = 'scout-handebol-v17';
 const URLS_TO_CACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
